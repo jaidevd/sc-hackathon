0b75f6d17c3ec4609f93bfad2ea8a279c03393f6
deshpande.jaidev@gmail.com <deshpande.jaidev@gmail.com>
2024-08-27 16:57:26 +0530

    ENH: KVP table and overlay SVG
    the SVG highlighter still needs to be connected to the click of the rows
    in the table, following which the correct page is to be rendered and the
    rectangle moved to the right place.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
+import re
 import glob
 import hashlib
 import json
 
+import gramex
+from gramex.config import secrets
+import pandas as pd
 from azure.ai.formrecognizer import DocumentAnalysisClient, AnalyzeResult
 from azure.core.credentials import AzureKeyCredential
 from tqdm import tqdm
+from tornado.gen import coroutine
 
 op = os.path
 
@@ -47,11 +52,93 @@ def ocr(
     return result
 
 
+def save(filename, body, **kwargs):
+    path = op.join("data", "upload", filename)
+    with open(path, "wb") as fout:
+        fout.write(body)
+    return path
+
+
+"""
+2.6 Non-mentioning of page numbers of annexures in the list of dates/ petition.
+2.8 Incorrect mentioning of description of annexures in Index
+3.2 Page No…………. not clear/legible/small font/dim/missing.
+3.3 Page Nos. �…..contain underlines/highlights/blanks/torn condition.
+5.5 Blanks in the affidavit
+8.1 Improper execution of Vakalatnama/Memo of Appearance
+17.1 Non-mentioning of the number of days of delay
+24.2 Non-inclusion of complete listing proforma filled in, signed in the paper-books
+"""
+
+
+def annexure_index(tables):
+    bad = []
+    for i, t in enumerate(tables):
+        clookup = pd.DataFrame(
+            [(c.row_index, c.column_index) for c in t.cells],
+            columns=['row', 'col']
+        )
+        for j, c in enumerate(t.cells):
+            if c.content.startswith('Annexure'):
+                raise ValueError('Needs more work.')
+                col_next = clookup.loc[j, 'col'] + 1
+                row = clookup[clookup['row'] == c.row_index]
+                cell_next = row[row['col'] == col_next]
+                if not re.match(r"\d+(-\d+)?", t.cells[cell_next.index[0]].content):
+                    bad.append({
+                        "table": i,
+                        "row": c.row_index,
+                        "col": c.column_index,
+                        "bbox": c.bounding_regions[0].to_dict()
+                    })
+    return bad
+
+
+def polygon2bbox(kvp):
+    kbr = kvp['key']['bounding_regions'][0]['polygon']
+    vbr = kvp['value']['bounding_regions'][0]['polygon']
+    bb = pd.DataFrame.from_records(kbr + vbr)
+    (xmin, ymin), (xmax, ymax) = bb.min(axis=0).values, bb.max(axis=0).values
+    return {"x": xmin, "y": ymin, "width": xmax - xmin, "height": ymax - ymin}
+
+
+def get_kvps(result, threshold=0.85, n_pages=20):
+    kvps = [kvp for kvp in result.key_value_pairs if kvp.confidence >= threshold and kvp.value]
+    kvps = pd.DataFrame.from_records([{
+        "key": kvp.key.content,
+        "value": kvp.value.content if kvp.value else "",
+        "page": kvp.key.bounding_regions[0].page_number,
+        "bbox": polygon2bbox(kvp.to_dict()),
+        "confidence": kvp.confidence
+    } for kvp in kvps])
+    kvps = kvps[kvps['value'] != ':unselected:']
+    kvps = kvps[kvps['value'] != 'N/A']
+    kvps.loc[kvps['value'] == ":selected:", "value"] = "Yes"
+    to_keep = pd.concat(
+        [kvps['key'].str.lower(), kvps['value'].str.lower()], axis=1).drop_duplicates().index
+    kvps = kvps.loc[to_keep]
+    # Drop short, common keys like "no.", "of", etc.
+    kvps = kvps[~kvps['key'].str.contains(r"^no\.$", case=False)]
+    kvps = kvps[~kvps['key'].str.contains(r"^on\.$", case=False)]
+    kvps = kvps[~kvps['key'].str.contains(r"^of$", case=False)]
+    if n_pages:
+        kvps = kvps[kvps['page'] <= n_pages]
+    return kvps.sort_values(["page", "confidence"], ascending=[True, False])
+
+
+@coroutine
+def extract(handler):
+    file = handler.request.files["file"][0]
+    path = save(**file)
+    result = yield gramex.service.threadpool.submit(ocr, path, endpoint=secrets["AZURE_OCR_URL"], api_key=secrets["AZURE_OCR_KEY"])
+    return {'url': path, 'kvps': get_kvps(result)}
+
+
 if __name__ == "__main__":
     from gramex import cache
-    secrets = cache.open('.secrets.yaml', 'config')
+    secrets_ = cache.open('.secrets.yaml', 'config')
     for file in tqdm(glob.glob("data/files/*.pdf")):
-        result = ocr(file, endpoint=secrets.AZURE_OCR_URL, api_key=secrets.AZURE_OCR_KEY)
+        result = ocr(file, endpoint=secrets_.AZURE_OCR_URL, api_key=secrets_.AZURE_OCR_KEY)
         kvps = []
         for kvp in result.key_value_pairs:
             if kvp.confidence >= 0.9:
